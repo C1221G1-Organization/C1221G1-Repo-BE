c67309d36b833c5fa8cd3ab67027e64817c2ce96
trần luật <luatzxcv@gmail.com>
2022-06-29 13:39:27 +0700

    LuatTN update DTO

--- pharmacy/src/main/java/com/c1221g1/pharmacy/repository/import_invoice/ISupplierRepository.java
+++ pharmacy/src/main/java/com/c1221g1/pharmacy/repository/import_invoice/ISupplierRepository.java
@@ -4,4 +4,18 @@ import com.c1221g1.pharmacy.entity.import_invoice.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ISupplierRepository extends JpaRepository<Supplier,String> {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
