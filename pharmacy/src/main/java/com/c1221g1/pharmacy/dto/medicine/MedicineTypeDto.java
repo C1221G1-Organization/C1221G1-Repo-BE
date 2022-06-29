d6f9a49363a1052975d99b1625cf6c3ee1e78a95
Hoang Long <longhoang6668@gmail.com>
2022-06-29 11:37:01 +0700

    LongNH update Dto

--- pharmacy/src/main/java/com/c1221g1/pharmacy/dto/medicine/MedicineTypeDto.java
+++ pharmacy/src/main/java/com/c1221g1/pharmacy/dto/medicine/MedicineTypeDto.java
@@ -1,30 +1,12 @@
 package com.c1221g1.pharmacy.dto.medicine;
 
-import com.c1221g1.pharmacy.entity.medicine.Medicine;
-
-import java.util.List;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MedicineTypeDto {
     private String medicineTypeId;
     private String medicineTypeName;
-    private List<Medicine> medicineList;
-
-    public MedicineTypeDto() {
-    }
-
-    public String getMedicineTypeId() {
-        return medicineTypeId;
-    }
-
-    public void setMedicineTypeId(String medicineTypeId) {
-        this.medicineTypeId = medicineTypeId;
-    }
-
-    public String getMedicineTypeName() {
-        return medicineTypeName;
-    }
-
-    public void setMedicineTypeName(String medicineTypeName) {
-        this.medicineTypeName = medicineTypeName;
-    }
 }
