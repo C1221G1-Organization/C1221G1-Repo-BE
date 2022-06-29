a043165d3a913ffd69192ecfd663e4b38058924b
ITlqad1699 <lqad1649engineer@gmail.com>
2022-06-29 16:24:20 +0700

    DaLQA update InvoiceController

--- pharmacy/src/main/java/com/c1221g1/pharmacy/service/ICrudService.java
+++
@@ -1,8 +0,0 @@
-package com.c1221g1.pharmacy.service;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ICrudService<E> {
-
-}
--- pharmacy/src/main/java/com/c1221g1/pharmacy/service/medicine/impl/MedicineTypeService.java
+++
@@ -1,8 +0,0 @@
-package com.c1221g1.pharmacy.service.medicine.impl;
-
-import com.c1221g1.pharmacy.service.medicine.IMedicineTypeService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MedicineTypeService implements IMedicineTypeService {
-}
