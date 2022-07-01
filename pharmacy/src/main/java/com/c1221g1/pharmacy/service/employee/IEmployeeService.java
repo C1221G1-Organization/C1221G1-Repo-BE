ecaf23a4090b4573267a5ab654d3af009a33553d
trangiang911 <tbgiang191199@gmail.com>
2022-07-01 13:46:53 +0700

    GiangTB update git

--- pharmacy/src/main/java/com/c1221g1/pharmacy/service/employee/IEmployeeService.java
+++ pharmacy/src/main/java/com/c1221g1/pharmacy/service/employee/IEmployeeService.java
@@ -6,12 +6,37 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface IEmployeeService {
+
+    /**
+     * this function use to get all page Employee
+     *
+     * @author GiangTB
+     * @Time 15:30 29/06/2022
+     */
     Page<Employee> getAllEmployee(String employeeIdVal, String employeeNameVal, String positionVal, String employeeAddressVal, String employeePhoneVal, Pageable pageable);
 
+    /**
+     * this function use to find Employee by id
+     *
+     * @author GiangTB
+     * @Time 17:00 29/06/2022
+     */
     Employee findEmployeeById(String id);
 
+    /**
+     * this function use to delete employee from page (not delete in database)
+     *
+     * @author GiangTB
+     * @Time 17:00 29/06/2022
+     */
     void deleteEmployeeById(String id);
 
+    /**
+     * this function use to get all list Employee
+     *
+     * @author GiangTB
+     * @Time 16:00 29/06/2022
+     */
     List<Employee> getListEmployee();
 
     void saveEmployee(Employee employee);
