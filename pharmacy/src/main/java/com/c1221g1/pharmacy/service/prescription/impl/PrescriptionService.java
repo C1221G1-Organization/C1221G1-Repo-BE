a0b97e244f7167578dfd81b1cfe694ea124873ce
ITlqad1699 <lqad1649engineer@gmail.com>
2022-07-06 07:35:52 +0700

    DaLQA update Be code manager_sale 06/07/2022

--- pharmacy/src/main/java/com/c1221g1/pharmacy/service/prescription/impl/PrescriptionService.java
+++ pharmacy/src/main/java/com/c1221g1/pharmacy/service/prescription/impl/PrescriptionService.java
@@ -1,6 +1,8 @@
 package com.c1221g1.pharmacy.service.prescription.impl;
 
 import com.c1221g1.pharmacy.dto.prescription.IMedicinePrescriptionDto;
+import com.c1221g1.pharmacy.dto.prescription.PrescriptionDetail;
+import com.c1221g1.pharmacy.dto.prescription.PrescriptionMedicineDetail;
 import com.c1221g1.pharmacy.entity.prescription.Prescription;
 import com.c1221g1.pharmacy.repository.prescription.IPrescriptionRepository;
 import com.c1221g1.pharmacy.service.prescription.IPrescriptionService;
@@ -45,6 +47,16 @@ public class PrescriptionService implements IPrescriptionService {
     @Override
     public IMedicinePrescriptionDto getPrescriptionById(String id) {
         return this.prescriptionRepository.detailPrescriptionById(id);
+    }
+
+    @Override
+    public PrescriptionDetail getDetailPrescription(String id) {
+        return this.prescriptionRepository.getDetailPrescription(id);
+    }
+
+    @Override
+    public List<PrescriptionMedicineDetail> getListPreMeDetail(String id) {
+        return this.prescriptionRepository.getDetailListPreMe(id);
     }
 
 }
