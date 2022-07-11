a0d95d654ca0d4aadf32e20703f770bacf333bc3
ITlqad1699 <lqad1649engineer@gmail.com>
2022-07-11 09:57:07 +0700

    DALQA

--- pharmacy/src/main/java/com/c1221g1/pharmacy/dto/trung_dto/MedicineDtoTrung.java
+++
@@ -1,8 +0,0 @@
-package com.c1221g1.pharmacy.dto.trung_dto;
-
-public class MedicineDtoTrung {
-    private String medicineId;
-    private String medicineName;
-    private Double medicineImportPrice;
-
-}
--- pharmacy/src/test/java/com/c1221g1/pharmacy/controller/invoice/InvoiceMedicineController_getListMedicine.java
+++ pharmacy/src/test/java/com/c1221g1/pharmacy/controller/invoice/InvoiceMedicineController_getListMedicine.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class InvoiceMedicine_getListMedicine {
+public class InvoiceMedicineController_getListMedicine {
     @Autowired
     private MockMvc mockMvc;
 
